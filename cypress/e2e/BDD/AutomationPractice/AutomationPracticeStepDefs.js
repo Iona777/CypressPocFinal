3fa2b72d7d549e18097c771376617ba2c98289cc
Greg.Macdonald <greg.macdonald77@gmail.com>
2025-01-29 10:39:35 +0000

    Add radio buttons 2 and 3

--- cypress/e2e/BDD/AutomationPractice/AutomationPracticeStepDefs.js
+++ cypress/e2e/BDD/AutomationPractice/AutomationPracticeStepDefs.js
@@ -1,3 +1,5 @@
+/// <reference types="Cypress"/>
+
 const { Given } = require("@badeball/cypress-cucumber-preprocessor");
 
 Given('I am on the Automation Practice page', function(){
@@ -5,9 +7,18 @@ Given('I am on the Automation Practice page', function(){
 
     //Set locator values
     cy.get("[id='radio-btn-example'] [value='radio1']").as('radioBtn1Locator')
+    cy.get("[id='radio-btn-example'] [value='radio2']").as('radioBtn2Locator')
+    cy.get("[id='radio-btn-example'] [value='radio3']").as('radioBtn3Locator')
 
 
-    //click on radio button 1
-    cy.get('@radioBtn1Locator').click()
+    //click on radio button 1, then button 2
+    cy.get('@radioBtn1Locator').click().should('be.checked')
+    cy.wait(1000)
+    cy.get('@radioBtn2Locator').click().should('be.checked')
+
+    //click on radio button 3
+    cy.get('@radioBtn3Locator').click().should('be.checked')
+    cy.get('@radioBtn1Locator').should('not.be.checked')
+    cy.get('@radioBtn2Locator').should('not.be.checked')
 
 })
