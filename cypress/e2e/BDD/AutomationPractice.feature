244dfd7beb636a5aa7516915304d7efe7e08b094
Greg.Macdonald <greg.macdonald77@gmail.com>
2025-01-31 16:30:00 +0000

    Start adding checkboxes

--- cypress/e2e/BDD/AutomationPractice.feature
+++ cypress/e2e/BDD/AutomationPractice.feature
@@ -4,3 +4,9 @@ Practice various Automation features
 
 Scenario: Select Radio buttons
 Given I am on the Automation Practice page
+And I select various radio buttons
+
+Scenario: Select Checkboxes
+Given I am on the Automation Practice page
+When I select checkbox 'Option1'
+
--- cypress/e2e/BDD/AutomationPractice/AutomationPracticeStepDefs.js
+++ cypress/e2e/BDD/AutomationPractice/AutomationPracticeStepDefs.js
@@ -1,15 +1,15 @@
 /// <reference types="Cypress"/>
-
-const { Given } = require("@badeball/cypress-cucumber-preprocessor");
+const { Given, When,Then, DataTable } = require("@badeball/cypress-cucumber-preprocessor");
 
 Given('I am on the Automation Practice page', function(){
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+})
 
+Given('I select various radio buttons', function(){
     //Set locator values
     cy.get("[id='radio-btn-example'] [value='radio1']").as('radioBtn1Locator')
     cy.get("[id='radio-btn-example'] [value='radio2']").as('radioBtn2Locator')
     cy.get("[id='radio-btn-example'] [value='radio3']").as('radioBtn3Locator')
-
 
     //click on radio button 1, then button 2
     cy.get('@radioBtn1Locator').click().should('be.checked')
@@ -22,3 +22,37 @@ Given('I am on the Automation Practice page', function(){
     cy.get('@radioBtn2Locator').should('not.be.checked')
 
 })
+
+When('I select checkbox {string}', function(option){
+    //Set locator values
+    cy.get("[id='checkbox-example'] [name='checkBoxOption1']").as('checkBox1Locator')
+    cy.get("[id='checkbox-example'] [name='checkBoxOption2'").as('checkBox2Locator')
+    cy.get("[id='checkbox-example'] [name='checkBoxOption3'").as('checkBox3Locator')
+
+    //Click on given checkbox
+    switch(option.toLowerCase())
+    {
+        case "option1":
+            cy.get('@checkBox1Locator').check().should('have.value', option.toLowerCase())
+            break;
+            case "option2":
+                cy.get('@checkBox2Locator').check().should('have.value', option.toLowerCase())
+                break;
+                case"option3":
+                cy.get('@checkBox3Locator').check().should('have.value', option.toLowerCase())
+                break;
+                default:
+                    cy.log("Invalid input "+option)
+    } 
+
+    
+})
+
+
+
+
+
+
+
+
+
