74410eb6b22d534465a75542040f0bedd908bbd4
Greg.Macdonald <greg.macdonald77@gmail.com>
2025-03-14 13:04:10 +0000

    Add mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -22,6 +22,15 @@ async function setupNodeEvents(on, config) {
 }
 
 module.exports = defineConfig({
+
+  reporter: 'cypress-mochawesome-reporter',
+
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: true
+  },
   
   env:{
     url: 'https://rahulshettyacademy.com',
@@ -33,6 +42,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     //specPattern: "cypress/e2e/*js",
     specPattern: "**/*.feature",
